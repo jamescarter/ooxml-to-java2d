9707a317551706a71cb04131e2de0b3da7f0ecb0
James Carter <james.carter@gmail.com>
2015-04-03 16:38:56 +0100

    Added SVG renderer

--- renderer-image/src/main/java/documentconverter/renderer/Image.java
+++ renderer-image/src/main/java/documentconverter/renderer/Image.java
@@ -3,23 +3,15 @@ package documentconverter.renderer;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-
 import documentconverter.renderer.Page;
 
 public class Image implements Page {
-	private static final float scale = 0.05f;
-	private BufferedImage bi;
 	private Graphics2D g;
 
-	public Image(int pageWidth, int pageHeight) {
-		bi = new BufferedImage((int) (pageWidth * scale), (int) (pageHeight * scale), BufferedImage.TYPE_INT_RGB);
-		g = (Graphics2D) bi.getGraphics();
+	public Image(Graphics2D g) {
+		this.g = g;
 
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, bi.getWidth(), bi.getHeight());
 		g.setColor(Color.BLACK);
-
 		g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_LCD_CONTRAST, 250);
@@ -32,15 +24,15 @@ public class Image implements Page {
 
 	@Override
 	public void setFontConfig(FontConfig fontConfig) {
-	    g.setFont(fontConfig.getFont().deriveFont(fontConfig.getSize() * scale));
+	    g.setFont(fontConfig.getFont());
 	}
 
 	@Override
 	public void drawString(String text, int x, int y) {
-		g.drawString(text, (int) (x * scale), (int) (y * scale));
+		g.drawString(text, x, y);
 	}
 
-	public BufferedImage getImage() {
-		return bi;
+	public Graphics2D getGraphics() {
+		return g;
 	}
 }
--- renderer-image/src/main/java/documentconverter/renderer/ImageRenderer.java
+++ renderer-image/src/main/java/documentconverter/renderer/ImageRenderer.java
@@ -1,5 +1,7 @@
 package documentconverter.renderer;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,13 +11,21 @@ import documentconverter.renderer.Page;
 import documentconverter.renderer.Renderer;
 
 public class ImageRenderer implements Renderer {
+	private static final float scale = 0.05f;
 	private List<BufferedImage> images = new ArrayList<>();
 
 	@Override
 	public Page addPage(int pageWidth, int pageHeight) {
-		Image page = new Image(pageWidth, pageHeight);
+		BufferedImage bi = new BufferedImage((int) (pageWidth * scale), (int) (pageHeight * scale), BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = (Graphics2D) bi.getGraphics();
 
-		images.add(page.getImage());
+		g.setBackground(Color.WHITE);
+		g.clearRect(0, 0, bi.getWidth(), bi.getHeight());
+		g.scale(scale, scale);
+
+		Image page = new Image(g);
+
+		images.add(bi);
 
 		return page;
 	}
